e9a105ecdacbd571172f0a070fb2840c3e472347
peszabolcs <perjesidev@gmail.com>
2024-05-06 20:52:44 +0200

    Add deposit function

---
+++ src/Deposit.java
@@ -0,0 +1,21 @@
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Deposit extends Account {
+    public Deposit(String name, String password, int id, int balance) {
+        super(name, password, id, balance);
+    }
+
+    public boolean deposit(int depositAmount) {
+        try {
+            setBalance(getBalance() + depositAmount);
+            Statement stmt = (Statement) Interface.getConnection().createStatement();
+            String sql = "UPDATE users SET balance = " + getBalance() + " WHERE idusers = " + getId();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+}
